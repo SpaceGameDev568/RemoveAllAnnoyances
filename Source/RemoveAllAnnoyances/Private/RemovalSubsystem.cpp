85981acc4cdc576706d37284903cf3d455c61300
Jesse Hodgson <space@spacegamedev.com>
2024-03-10 12:55:42 -0400

    v1.2.6

--- Source/RemoveAllAnnoyances/Private/RemovalSubsystem.cpp
+++ Source/RemoveAllAnnoyances/Private/RemovalSubsystem.cpp
@@ -1,10 +1,16 @@
 // Copyright 2024 Jesse Hodgson.
 
 #include "RemovalSubsystem.h"
+
+#include "FGHealthComponent.h"
 #include "RemoveAllAnnoyances.h"
 #include "RemoveAllAnnoyances_ConfigStruct.h"
 #include "Kismet/GameplayStatics.h"
 #include "FGItemPickup.h"
+#include "ModLoading/ModLoadingLibrary.h"
+#include "SessionSettings/SessionSettingsSubsystem.h"
+#include "SML/Public/Player/SMLRemoteCallObject.h"
+#include "FGPlayerController.h"
 
 ARemovalSubsystem::ARemovalSubsystem()
 {
@@ -35,56 +41,180 @@ void ARemovalSubsystem::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogRemoveAllAnnoyances, Verbose, TEXT("RemoveAllAnnoyances Subsystem loaded successfully!"));
+	UE_LOG(LogRemoveAllAnnoyances, Verbose, TEXT("Loaded RemoveAllAnnoyances subsystem successfully!"));
 
-	FTimerHandle MemberTimerHandle;
+	FModInfo ModInfo;
 
-	TSubclassOf<AActor> ActorClass;
-	bool ShouldSpawnReward = false;
+	const UGameInstance* GameInstance = GetGameInstance();
 
-	GetWorldTimerManager().SetTimer(MemberTimerHandle, this, &ARemovalSubsystem::RunRemover(ActorClass, ShouldSpawnReward), DeletionInterval, true, DeletionInterval);
-}
+	UModLoadingLibrary *ModLoadingLibrary = GameInstance->GetSubsystem<UModLoadingLibrary>();
 
-void ARemovalSubsystem::RunRemover(TSubclassOf<AActor> ActorClass, bool ShouldSpawnReward)
-{
+	ModLoadingLibrary->GetLoadedModInfo("RemoveAllAnnoyances", ModInfo);
 
-	TArray<AActor*> OutActors;
-	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ActorClass, OutActors);
+	// Log the name and version of the mod
+	UE_LOG(LogRemoveAllAnnoyances, Verbose, TEXT("%s"), *ModInfo.FriendlyName.Append(", " + ModInfo.Version.ToString()));
 
-	FString ActorName = ActorClass->GetName();
+	TArray<TSubclassOf<AActor>> AnnoyanceList;
+	TArray<TSubclassOf<AFGCharacterBase>> CharacterAnnoyanceList;
 
-	if(OutActors.Num() == 0)
+	// Add all annoyances to list
+	if (RemoveArtifacts)
 	{
-		UE_LOG(LogRemoveAllAnnoyances, Verbose, TEXT("Found none of type %s"), *ActorName);
+		AnnoyanceList.Add(Wat1);
+
+		UE_LOG(LogRemoveAllAnnoyances, Warning, TEXT("Added Artifacts to the list of annoyances to remove!"));
+	}
+	if(RemoveMantaRays)
+	{
+		AnnoyanceList.Add(MantaRay);
+
+		UE_LOG(LogRemoveAllAnnoyances, Warning, TEXT("Added MantaRays to the list of annoyances to remove!"));
+	}
+	if (RemoveDeposits)
+	{
+		AnnoyanceList.Add(Deposit);
+
+		UE_LOG(LogRemoveAllAnnoyances, Warning, TEXT("Added Deposits to the list of annoyances to remove!"));
+	}
+	if (RemoveStingers)
+	{
+		CharacterAnnoyanceList.Add(EliteStinger);
+		CharacterAnnoyanceList.Add(AlphaStinger);
+		CharacterAnnoyanceList.Add(ChildStinger);
+
+		UE_LOG(LogRemoveAllAnnoyances, Warning, TEXT("Added Stingers to the list of annoyances to remove!"));
+	}
+	if (RemoveGasRocks)
+	{
+		AnnoyanceList.Add(SporeFlower);
+		AnnoyanceList.Add(GasPillar1);
+		AnnoyanceList.Add(GasPillar2);
+		AnnoyanceList.Add(GasPillar3);
+		AnnoyanceList.Add(GasPillar4);
+		AnnoyanceList.Add(GasPillar5);
+
+		UE_LOG(LogRemoveAllAnnoyances, Warning, TEXT("Added Gas Rocks to the list of annoyances to remove!"));
+	}
+	if (RemoveHogs)
+	{
+		CharacterAnnoyanceList.Add(AlphaHog);
+		CharacterAnnoyanceList.Add(CliffHog);
+		CharacterAnnoyanceList.Add(NuclearHog);
+		CharacterAnnoyanceList.Add(Hog);
+
+		UE_LOG(LogRemoveAllAnnoyances, Warning, TEXT("Added Hogs to the list of annoyances to remove!"));
+	}
+	if (RemoveCrabs)
+	{
+		AnnoyanceList.Add(CrabHatcher);
+
+		UE_LOG(LogRemoveAllAnnoyances, Warning, TEXT("Added Crabs to the list of annoyances to remove!"));
+	}
+	if (RemoveSpitters)
+	{
+		CharacterAnnoyanceList.Add(AquaticAlphaSpitter);
+		CharacterAnnoyanceList.Add(AquaticSpitter);
+		CharacterAnnoyanceList.Add(DesertAlphaSpitter);
+		CharacterAnnoyanceList.Add(DesertSpitter);
+		CharacterAnnoyanceList.Add(RedForestAlphaSpitter);
+		CharacterAnnoyanceList.Add(RedForestSpitter);
+		CharacterAnnoyanceList.Add(AlphaForestSpitter);
+		CharacterAnnoyanceList.Add(ForestSpitter);
+
+		UE_LOG(LogRemoveAllAnnoyances, Warning, TEXT("Added Spitters to the list of annoyances to remove!"));
+	}
+	if (RemoveDestructibleRocks)
+	{
+		AnnoyanceList.Add(DestructibleRock1);
+		AnnoyanceList.Add(DestructibleRock2);
+		AnnoyanceList.Add(DestructibleRockFlat);
+
+		UE_LOG(LogRemoveAllAnnoyances, Warning, TEXT("Added Destructible Rocks to the list of annoyances to remove!"));
+	}
+	if (RemoveBeans)
+	{
+		CharacterAnnoyanceList.Add(SpaceGiraffe);
+
+		UE_LOG(LogRemoveAllAnnoyances, Warning, TEXT("Added Beans to the list of annoyances to remove!"));
+	}
+
+	if (AnnoyanceList.Num() == 0 && CharacterAnnoyanceList.Num() == 0)
+	{
+		UE_LOG(LogRemoveAllAnnoyances, Verbose, TEXT("No annoyances selected!"));
+
+		APlayerController* PlayerControllerRef = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+		AFGPlayerController* VarPlayerController = Cast<AFGPlayerController>(PlayerControllerRef);
+
+		USMLRemoteCallObject *SMLRemoteCallObject = VarPlayerController->GetRemoteCallObjectOfClass<USMLRemoteCallObject>();
+
+		SMLRemoteCallObject->SendChatMessage("Remove All Annoyances: No annoyances selected! Please go to the Main Menu and select \"Mods\" from the bottom of the menu and choose \"Remove All Annoyances\". Then scroll down and select some annoyances to remove!", ErrorMessageColor);
 	}
 	else
 	{
-		if(ShouldSpawnReward)
+		FTimerHandle MemberTimerHandle;
+
+		GetWorldTimerManager().SetTimer(MemberTimerHandle, [this, AnnoyanceList, CharacterAnnoyanceList](){RunRemover(AnnoyanceList, CharacterAnnoyanceList);}, DeletionInterval, true, DeletionInterval);
+	}
+}
+
+void ARemovalSubsystem::RunRemover(TArray<TSubclassOf<AActor>> AnnoyanceList, TArray<TSubclassOf<AFGCharacterBase>> CharacterAnnoyanceList)
+{
+	TArray<AActor*> OutActors;
+
+	for (auto&  CurrentActorType : AnnoyanceList)
+	{
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), CurrentActorType, OutActors);
+
+		if(OutActors.Num() == 0)
 		{
-			// int i = 0; i < OutActors.Num(); i++)
-			for (auto&  Actor: OutActors)
-			{
-				TSubclassOf<class AFGItemPickup> RewardBlueprint;
-
-				static ConstructorHelpers::FObjectFinder<UBlueprint> ItemBlueprint(TEXT("Blueprint'/Game/FactoryGame/Resource/Environment/Crystal/BP_Crystal_mk2.BP_Crystal_mk2'"));
-				if (ItemBlueprint.Object){
-					RewardBlueprint = (UClass*)ItemBlueprint.Object->GeneratedClass;
-				}
-
-				FActorSpawnParameters SpawnParams;
-				SpawnParams.Owner = this;
-
-				GetWorld()->SpawnActor<AFGItemPickup>(RewardBlueprint, Actor->GetActorLocation(), Actor->GetActorRotation(), SpawnParams);
-			}
+			UE_LOG(LogRemoveAllAnnoyances, Verbose, TEXT("Found none of type %s"), *CurrentActorType->GetName());
 		}
 		else
 		{
-			// int i = 0; i < OutActors.Num(); i++)
+			UE_LOG(LogRemoveAllAnnoyances, Verbose, TEXT("Found %d of type %s"), OutActors.Num(), *CurrentActorType->GetName());
+
 			for (auto&  Actor: OutActors)
 			{
+				// Spawn reward for alien artifacts
+				if(CurrentActorType == Wat1)
+				{
+					FActorSpawnParameters SpawnParams;
+					SpawnParams.Owner = this;
+
+					GetWorld()->SpawnActor<AFGItemPickup>(Reward, Actor->GetActorLocation(), Actor->GetActorRotation(), SpawnParams);
+				}
 				Actor->Destroy();
 			}
 		}
+	}
+	// Kill creatures before destroying them
+	for(auto& CurrentCharacterType : CharacterAnnoyanceList)
+	{
 
+		TArray<AFGCharacterBase*> OutCharacters;
+		TArray<AActor*> OutActorCharacters;
+
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), CurrentCharacterType, OutActorCharacters);
+
+		if(OutActorCharacters.Num() == 0)
+		{
+			UE_LOG(LogRemoveAllAnnoyances, Verbose, TEXT("Found none of type %s"), *CurrentCharacterType->GetName());
+		}
+		else
+		{
+			UE_LOG(LogRemoveAllAnnoyances, Verbose, TEXT("Found %d of type %s"), OutCharacters.Num(), *CurrentCharacterType->GetName());
+
+			for (auto& Character : OutActorCharacters)
+			{
+				AFGCharacterBase* CastedCharacter = Cast<AFGCharacterBase>(Character);
+
+				CastedCharacter->GetHealthComponent()->Kill();
+
+				if (IsValid(Character))
+				{
+					Character->Destroy();
+				}
+			}
+		}
 	}
 }
--- Source/RemoveAllAnnoyances/Public/RemovalSubsystem.h
+++ Source/RemoveAllAnnoyances/Public/RemovalSubsystem.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "FGCharacterBase.h"
 #include "Subsystem/ModSubsystem.h"
 #include "RemoveAllAnnoyances_ConfigStruct.h"
 #include "RemovalSubsystem.generated.h"
@@ -31,11 +32,148 @@ public:
 
 	float DeletionInterval;
 
+// Error Message Color
+
+	UPROPERTY(EditAnywhere)
+	FLinearColor ErrorMessageColor = FLinearColor::Red;
+
+// Reward Class
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> Reward;
+
+// Annoyances Blueprint Classes
+
+	// Mercer Sphere/Somersloop
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> Wat1;
+
+	// Manta Ray
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> MantaRay;
+
+	// Deposits
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> Deposit;
+
+	// Stinger Subclasses
+
+		// Elite Stinger
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> EliteStinger;
+
+		// Alpha Stinger
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> AlphaStinger;
+
+		// Child Stinger
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> ChildStinger;
+
+	// Gas Pillar Subclasses
+
+		// Spore Flower
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> SporeFlower;
+
+		// Gas Pillar 1
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> GasPillar1;
+
+		// Gas Pillar 2
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> GasPillar2;
+
+		// Gas Pillar 3
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> GasPillar3;
+
+		// Gas Pillar 4
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> GasPillar4;
+
+		// Gas Pillar 5
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> GasPillar5;
+
+	// Hog Subclasses
+
+		// Alpha Hog
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> AlphaHog;
+
+		// Cliff Hog
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> CliffHog;
+
+		// Nuclear Hog
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> NuclearHog;
+
+		// Hog
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> Hog;
+
+	// Crab Hatcher
+	UPROPERTY(EditAnywhere)
+    TSubclassOf<AActor> CrabHatcher;
+
+    // Plasma Spitter Subclasses
+
+		// Aquatic Alpha Spitter
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> AquaticAlphaSpitter;
+
+		// Aquatic Spitter
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> AquaticSpitter;
+
+	    // Desert Alpha Spitter
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> DesertAlphaSpitter;
+
+		// Desert Spitter
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> DesertSpitter;
+
+	    // Red Forest Alpha Spitter
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> RedForestAlphaSpitter;
+
+		// Red Forest Spitter
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> RedForestSpitter;
+
+	    // Alpha Forest Spitter
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> AlphaForestSpitter;
+
+		// Forest Spitter
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AFGCharacterBase> ForestSpitter;
+
+	// Destructible Rock Subclasses
+
+		// Destructible Rock 1
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> DestructibleRock1;
+
+		// Destructible Rock 2
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> DestructibleRock2;
+
+		// Destructible Rock Flat
+		UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> DestructibleRockFlat;
+
+	// Space Giraffe
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AFGCharacterBase> SpaceGiraffe;
+
 	UFUNCTION()
-	void RunRemover(TSubclassOf<AActor> ActorClass, bool ShouldSpawnReward);
+	void RunRemover(TArray<TSubclassOf<AActor>> AnnoyanceList, TArray<TSubclassOf<AFGCharacterBase>> CharacterAnnoyanceList);
 
 protected:
 
 	virtual void BeginPlay() override;
-	
 };
